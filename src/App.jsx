b0196c5c36363163497c615fc4def1ea8cf9e023
Denis Tsymbaliuk <denis.tsymbaliuk95@gmail.com>
2025-01-07 20:35:45 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Profile from "./components/Profile/Profile";
+import userData from "./userData.json"
 
 const App = () => {
   return (
     <>
-      <Profile/>
-    </>
+      <Profile userData={userData}/>    
+    </> 
   );
 };
 
