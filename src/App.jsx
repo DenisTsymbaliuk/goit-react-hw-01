36217b1a076fb0da16d02adea7374bd9c884c9c1
Denis Tsymbaliuk <denis.tsymbaliuk95@gmail.com>
2025-01-10 17:26:14 +0100

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Profile from "./components/Profile/Profile";
 import userData from "./userData.json"
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./transactions.json"
+
 const App = () => {
   return (
     <>
-      <Profile userData={userData}/>    
+      <Profile userData={userData} />  
+      <FriendList friends={friends} />
+       <TransactionHistory items={transactions} />
     </> 
   );
 };
